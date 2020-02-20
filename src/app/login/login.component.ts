ae13f78b63805f4afcba8807ccb6b39feb036bfd
Leonardo Donato <leo_donato007@yahoo.com.br>
2020-02-20 11:45:57 -0300

    Alterações em login e implementando opção de revelar senha

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -14,6 +14,10 @@ export class LoginComponent implements OnInit {
 
   public isInvalid: boolean = false;
 
+  public changeEye: boolean = false;
+
+  public changeButton: boolean = false;
+
   constructor(
     private fb: FormBuilder,
     private authService: AuthService,
@@ -31,20 +35,34 @@ export class LoginComponent implements OnInit {
   login() {
     const email = this.formLogin.get('email').value;
     const senha = this.formLogin.get('senha').value;
-
+    this.changeButton = true;
     this.authService
     .authenticate(email, senha)
     .subscribe(
-      () => this.router.navigate(['eventos']),
+      () => {
+        this.router.navigate(['eventos']);
+        this.changeButton = false;
+      },
       err => {
         console.log(err);
         this.formLogin.reset();
+        this.changeButton = false;
         this.isInvalid = true;
         setTimeout(() => {
           this.isInvalid = false
         }, 4000);
       }
     )
+  }
+
+  showPassword(password: any){
+    if(password.type === 'password'){
+      password.type = 'text';
+      this.changeEye = true;
+    } else{
+      password.type = 'password';
+      this.changeEye = false;
+    }
   }
 
 }
